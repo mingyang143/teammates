6f82bfbcc0f86916d5d77c022f9401f81f43209d
Mingyang <limingyanglmy10@gmail.com>
2024-12-14 00:17:10 +0800

    Add tests for visibility-messages

--- src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
@@ -5,4 +5,49 @@ describe('VisibilityCapabilityPipe', () => {
     const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return the correct message when all controls are true', () => {
+    const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
+    const controls = {
+      SHOW_RESPONSE: true,
+      SHOW_RECIPIENT_NAME: true,
+      SHOW_GIVER_NAME: true,
+    };
+    const result = pipe.transform(controls);
+    expect(result).toBe('can see your response, the name of the recipient, and your name');
+  });
+
+  it('should return the correct message when only SHOW_RECIPIENT_NAME is true', () => {
+    const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
+    const controls = {
+      SHOW_RESPONSE: true,
+      SHOW_RECIPIENT_NAME: true,
+      SHOW_GIVER_NAME: false,
+    };
+    const result = pipe.transform(controls);
+    expect(result).toBe('can see your response, the name of the recipient, but not your name');
+  });
+
+  it('should return the correct message when only SHOW_GIVER_NAME is true', () => {
+    const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
+    const controls = {
+      SHOW_RESPONSE: true,
+      SHOW_RECIPIENT_NAME: false,
+      SHOW_GIVER_NAME: true,
+    };
+    const result = pipe.transform(controls);
+    expect(result).toBe('can see your response, and your name, but not the name of the recipient');
+  });
+
+  it('should return the correct message when all controls are false', () => {
+    const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
+    const controls = {
+      SHOW_RESPONSE: false,
+      SHOW_RECIPIENT_NAME: false,
+      SHOW_GIVER_NAME: false,
+    };
+    const result = pipe.transform(controls);
+    expect(result).toBe('can see your response, but not the name of the recipient, or your name');
+  });
+
 });
+
--- src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
@@ -1,8 +1,73 @@
-import { VisibilityEntityNamePipe } from './visibility-entity-name.pipe';
+import {VisibilityEntityNamePipe} from './visibility-entity-name.pipe';
+import {
+    FeedbackParticipantType,
+    FeedbackVisibilityType,
+    NumberOfEntitiesToGiveFeedbackToSetting
+} from "../../../types/api-output";
 
 describe('VisibilityEntityNamePipe', () => {
   it('create an instance', () => {
     const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return the correct entity name when the visibility type is RECIPIENT and recipient type is ' +
+      'INSTRUCTORS ', () => {
+    const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+    const result = pipe.transform(FeedbackVisibilityType.RECIPIENT, FeedbackParticipantType.INSTRUCTORS);
+    expect(result).toBe('The receiving instructor');
+  });
+
+  it('should return the correct entity name when the visibility type is RECIPIENT and recipient type is ' +
+      'STUDENTS', () => {
+    const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+    const result = pipe.transform(FeedbackVisibilityType.RECIPIENT, FeedbackParticipantType.STUDENTS);
+    expect(result).toBe('The receiving student');
+  });
+
+  it('should return the correct entity name when the visibility type is RECIPIENT and recipient type is ' +
+      'TEAMS', () => {
+    const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+    const result = pipe.transform(FeedbackVisibilityType.RECIPIENT, FeedbackParticipantType.TEAMS);
+    expect(result).toBe('The receiving team');
+  });
+
+    it('should return the correct entity name when the visibility type is RECIPIENT and recipient type is ' +
+        'NONE', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.RECIPIENT, FeedbackParticipantType.NONE);
+        expect(result).toBe('unknown');
+    });
+
+    it('should return the correct entity name when the visibility type is GIVEN_TEAM_MEMBERS', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.GIVER_TEAM_MEMBERS);
+        expect(result).toBe('Your team members');
+    });
+
+    it('should return the correct entity name when the visibility type is INSTRUCTORS', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.INSTRUCTORS);
+        expect(result).toBe('Instructors in this course');
+    });
+
+    it('should return the correct entity name when the visibility type is STUDENTS', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.STUDENTS);
+        expect(result).toBe('Other students in the course');
+    });
+
+    it('should return the correct entity name when the visibility type is RECIPIENT_TEAM_MEMBERS', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.RECIPIENT_TEAM_MEMBERS);
+        expect(result).toBe("The recipient's team members");
+    });
+
+    it('should return plural form of entity name when the visibility type is RECIPIENT and the number of entities ' +
+        'to give feedback to UNLIMITED', () => {
+        const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+        const result = pipe.transform(FeedbackVisibilityType.RECIPIENT, FeedbackParticipantType.STUDENTS,
+            NumberOfEntitiesToGiveFeedbackToSetting.UNLIMITED);
+        expect(result).toBe('The receiving students');
+    });
 });
